2fa087890d28ab00e5d0ed38395eb33fc786a1fc
MaikeraW <wagner.maiconhenrique@gmail.com>
2026-05-14 10:29:52 -0300

    Importado elementos da biblioteca e inicio do projeto

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
+import { BrowserRouter, Route, Router, Link } from 'react-router-dom'
+
 import './App.css'
 
 function App() {
 
   return (
-    <>
-      
-    </>
+    <BrowserRouter>
+      <nav>
+        <ul>
+          <li> <Link to="/"> Home </Link> </li>
+        </ul>
+      </nav>
+    
+    </BrowserRouter>
   )
 }
 
